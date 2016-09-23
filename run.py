d3c71b84e0c184c9e917c4faac00d130f7f8be88
LilLozc <LilLozc@gmail.com>
2016-09-23 18:46:40 +1000

    will now detect host path to use

--- run.py
+++ run.py
@@ -3,13 +3,12 @@ import threading
 import os
 
 
-path_to_runner = "./x64/Debug/SCP.exe"
-#path_to_runner = "./Debug/SCP.exe"
-#TODO:  Lauren - put in a check to see which path to use depending on host machine
+path_to_runner_1 = "./x64/Debug/SCP.exe"
+path_to_runner_2 = "./Debug/SCP.exe"
 path_to_test_files = "./test_data/"
 path_to_output = "./output/"
 
-def launch_scp_program(filename):
+def launch_scp_program(path_to_runner,filename):
     print("Launching SCP with %s" % filename)
     # Start the process with the filename and args
     subprocess.call([path_to_runner, path_to_test_files+filename, "test"])
@@ -19,10 +18,22 @@ def launch_scp_program(filename):
     # Notify with any output
 
 
+def host_path():
+    if os.path.exists(path_to_runner_1):
+        print("using host path %s\n" % path_to_runner_1)
+        return path_to_runner_1
+    elif os.path.exists(path_to_runner_2):
+        print("using host path %s\n" % path_to_runner_2)
+        return path_to_runner_2
+
 if __name__ == "__main__":
-    print("Launching tests")
+
+    print("Launching tests\n")
+    path_to_runner = host_path()
     for filename in os.listdir(path_to_test_files):
-        launch_scp_program(filename)
+       launch_scp_program(path_to_runner,filename)
        #thread = threading.Thread(target=launch_scp_program, args=(filename,))
        #thread.daemon = True
        #thread.start()
+
+    print("end tests")

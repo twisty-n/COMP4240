429be82c9f2404ae0d458bd59e0936e4affeb7ee
LilLozc <LilLozc@gmail.com>
2016-09-24 11:40:24 +1000

    basic csv writing working -by row with lists

--- run.py
+++ run.py
@@ -8,16 +8,34 @@ path_to_runner = "./Debug/SCP.exe"
 path_to_runner_x64 = "./x64/Debug/SCP.exe"
 path_to_test_files = "./test_data/"
 path_to_output = "./output/"
+summary_output_file = "summary.csv"
+
+
+# lists are required to store data - as this works best with the
+# csv module - however, I'm not sure if this is going to be the best
+# way to track the data.  We would need one list per row.
+# I will look into iterable data structures for storing and writing to csv
+# but at the moment, this is a 'working' csv write solution
+best_cover_list = []
+best_cover_time_list = []
+average_cover_list = []
+average_cover_time = []
+
+
 
 def launch_scp_program(path_to_runner,filename):
     print("Launching SCP with %s" % filename)
     # Start the process with the filename and args
-    subprocess.call([path_to_runner, path_to_test_files+filename, "test"])
+    test_scp = subprocess.call([path_to_runner, path_to_test_files+filename, "test"])
     # Listen to the results
 
+    #TODO:  Lauren - still trying to work this out.  Also need to take to twisty about best design for getting data -  might be best to wait and see if I can get csv writing to work with an iterable 2D array first
 
-    # Notify with any output
-
+    #proof of concept - appending data to lists at this point
+    best_cover_list.append(0)
+    best_cover_time_list.append(0)
+    average_cover_list.append(0)
+    average_cover_time.append(0)
 
 def host_path():
     if os.path.exists(path_to_runner):
@@ -28,6 +46,23 @@ def host_path():
         print("path to SCP executable not found\nenvironment settings are not correct\ncontact your support administrator for further details")
         exit()
 
+#for debug
+def print_lists_to_console():
+    print "best_cover_list: ", best_cover_list
+    print "best_cover_time_list: ", best_cover_time_list
+    print "average_cover_list: ", average_cover_list
+    print "average_cover_time: ", average_cover_time
+
+
+def append_to_csv():
+    with open(path_to_output+summary_output_file, 'a') as csvfile:
+        wr = csv.writer(csvfile, dialect='excel', delimiter=',', quoting=csv.QUOTE_NONE, quotechar='', escapechar='')
+        wr.writerow(best_cover_list)
+        wr.writerow(best_cover_time_list)
+        wr.writerow(average_cover_list)
+        wr.writerow(average_cover_time)
+    csvfile.close()
+
 if __name__ == "__main__":
 
     print("Launching tests\n")
@@ -37,5 +72,7 @@ if __name__ == "__main__":
        #thread = threading.Thread(target=launch_scp_program, args=(filename,))
        #thread.daemon = True
        #thread.start()
+
+    append_to_csv()
 
     print("end tests")

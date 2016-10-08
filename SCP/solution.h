f394c3271233c08eec79663284bd1b0384e46ff5
Tristan Newmann <tnew2294@gmail.com>
2016-10-08 14:12:54 +1100

    Add extra solution field

--- SCP/solution.h
+++ SCP/solution.h
@@ -2,9 +2,33 @@
 
 struct Solution {
 	int cost;
+
+	/*
+		Each cell represents a row, the value represents the column covering that row
+	*/
 	int * covering_columns;
+
+	/*
+		An array holding the minimal columns required for the solution
+		Holds the same information as columns_in_solution, but is 
+		more of a list, where the unfilled indexes are not part of the solution
+	*/
 	int * minimal_cover;
+
+	/*
+		Each cell represents a column, a value of 1 indicates a column is in the solution
+		a value of 0 indicates a column is outside of the solution
+	*/
+	int * columns_in_solution;
+
+	/*
+		The number of columns in the solution (useful for unicost SCP)
+	*/
 	int number_of_covers;
+
+	/*
+		The runtime to generate this solution
+	*/
 	double time;
 };
 

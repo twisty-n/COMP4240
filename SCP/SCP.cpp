790532544bc5cd90d1738df5a7f4638b9fe7d89a
LilLozc <LilLozc@gmail.com>
2016-11-17 14:50:23 +1100

    greedy and first accept are working

--- SCP/SCP.cpp
+++ SCP/SCP.cpp
@@ -136,7 +136,8 @@ int main(int argument_count, char * argv[])
 			current_solution = *returned;
 			break;
 		case 4:
-			random_construction(&instance, &current_solution);	//TRUE == unicost, FALSE == NON-UNICOST
+			//random_construction(&instance, &current_solution);
+			greedy_construction(&instance, &current_solution, FALSE);	//TRUE == unicost, FALSE == NON-UNICOST
 			//printout required for python report
 			if (i == 0) {
 				printf("%d ", current_solution.cost);
